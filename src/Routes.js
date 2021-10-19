2b60cb6d94fc65f8288dc941150e0511ecbe8470
려니맥 <ryeonimaeg@lyeonimaeg-ui-MacBookPro.local>
2021-10-20 00:48:23 +0900

    Refactor: 코드리뷰반영과이벤트추가완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-
+import Footer from './components/Footer/Footer';
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
       </Switch>
+      <Footer />
     </Router>
   );
 };
